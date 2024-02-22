428979f46adff1055211720889448c81a9d4006d
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-02-22 11:58:27 +0000

    Add question and answers option in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 const question = [
     {
-        question: ""
+        question: "Your passenger wants to discuss something with you during the journey. what should you do?",
+        answers: [
+            {text: "Concentrate on the discussion", correct: false},
+            {text: "Concentrate on the driving", correct: true},
+            {text: "Concentrate on the discussion and driving", correct: false},
+        ]
+    },
+    {
+        question: "What could cause the vehicle to leave the road?",
+        answers: [
+            {text: "Tiredness", correct: false},
+            {text: "Distraction", correct: false},
+            {text: "Inattention", correct: true},
+        ]
+    },
+    {
+        question: "What can inpair fitness to drive?",
+        answers: [
+            {text: "Fatigue", correct: true},
+            {text: "Certain medicines", correct: true},
+            {text: "Alcohol and other intoxicants", correct: true},
+        ]        
     }
 ]
